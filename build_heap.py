ddf04fe683c8b1826619a6c7e5bbd24423282ad0
OlegsJanuskevics221RDC059 <124894319+OlegsJanuskevics221RDC059@users.noreply.github.com>
2023-03-20 01:11:47 +0000

    Upgraded code Oļegs Januškevičs 221RDC059

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,36 @@
 # python3
-
+from math import floor
 
 def build_heap(data):
+    count=0
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
+    size = n-1
+    for i in range (floor(n/2), -1, -1):
+        swaps = sift_down(i, size, data, swaps, count)
+    return swaps, count
 
+def sift_down(i, size, data, swaps):
+    minIndex = i
+    l = LeftChild(i+1)
+    if l <= size and data[l] < data[minIndex]:
+        minIndex = l
+    r = RightChild(i+1)
+    if r <= size and data[r] < data[minIndex]:
+        minIndex = r
+    if i != minIndex:
+        data[i], data[minIndex] = data[minIndex], data[i]
+        count = count+1
+        swaps.append((i,minIndex))
+        swaps, count, data = sift_down(minIndex, size,data, swaps, count)
+    return swaps, count, data
 
-    return swaps
+def LeftChild(i):
+    return 2*i-1
+
+def RightChild(i):
+    return 2*i
 
 
 def main():
@@ -15,7 +38,6 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
 
     # input from keyboard
     n = int(input())
